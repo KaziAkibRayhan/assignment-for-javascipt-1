3219b3504f5a693f21620a80b22ba9fe8591be4c
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2021-01-22 22:17:09 +0600

    Update Code

--- what.js
+++
@@ -1,7 +0,0 @@
-function KilometerToMeter(numberOfKilometer) // Set parameter
-{
-   var result = numberOfKilometer * 1000;
-    return result;
-}
-var result = kilometerToMeter(8);
-console.log(result);
